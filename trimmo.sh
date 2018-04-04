361cf7cd64413837c3366db4012259a5083a69fb
clogan88 <clogan@csumb.edu>
2018-04-03 17:05:45 -0700

    commented trimmo.sh file; maybe make Rmd ??

--- trimmo.sh
+++ trimmo.sh
@@ -19,3 +19,16 @@ done;
 # using MacManes default parameters in trinity
 # single end file only
 # java -jar /opt/Trimmomatic-0.35/trimmomatic-0.35.jar SE -phred33 CLMP001A_S52_L005_R1_001.fastq CLMP001A_S52_L005_R1_001_trimmo.fastq ILLUMINACLIP:/opt/Trimmomatic-0.35/adapters/TruSeq3-SE.fa:2:30:10 SLIDINGWINDOW:4:5 LEADING:5 TRAILING:5 MINLEN:25
+
+# Trimmomatic manual here:
+# http://www.usadellab.org/cms/uploads/supplementary/Trimmomatic/TrimmomaticManual_V0.32.pdf
+
+# This will perform the following steps in this order:
+# 1. Remove Illumina adapters provided in TruSeq3-SE.fa (provided)
+#    note: Trimmomatic will look for seed matches (16 bases) allowing maximally 2 mismatches. 
+#    These seeds will be extended and clipped if in the case of paired end reads a score of 
+#    30 is reached (about 50 bases), or in the case of single ended reads a score of 10 (about 17 bases)
+# 2. Remove leading low quality or N bases (below quality 5)
+# 3. Remove trailing low quality or N bases (below quality 5)
+# 4. Scan the read with a 4 base wide sliding window, cutting when the average quality per base drops below 5
+# 5. Drop reads which are less than 25 bases long after these steps
